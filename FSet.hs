cb1911499984f323d5980e67120f8c193ec81508
Alexander Vieth <alex@lagoa.com>
2015-03-03 00:04:45 -0500

    experiments

--- FSet.hs
+++ FSet.hs
@@ -8,8 +8,16 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverlappingInstances #-}
+-- Incoherent instances used for IxEither... is this ok?
+-- Maybe! I think the issue is that a t can appear more than once in a list
+-- ts, and so there really isn't a way to disambiguate the CElem choice.
+-- But that should be fine... we don't care where in the list the thing is
+-- found, so long as it's found.
+{-# LANGUAGE IncoherentInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RebindableSyntax #-}
 
+import Prelude hiding (return, (>>=), (>>))
 import Data.Proxy
 import Data.Type.Equality
 import Data.TypeNat.Nat
@@ -40,9 +48,6 @@ instance CElem t (t ': ts) where
 instance (CElem t ts) => CElem t (r ': ts) where
   celemFind ifThere ifHere simplify x =
     ifThere x (celemFind ifThere ifHere simplify (simplify x))
-
-type family Hd (l :: [k]) :: k where
-  Hd (t ': ts) = t
 
 data TList :: [*] -> * where
   EmptyTList :: TList '[]
@@ -159,3 +164,100 @@ exampleOneOf1 = ThisOne True
 
 exampleOneOf2 :: OneOf '[Int, Bool, String]
 exampleOneOf2 = ThisOne "foobar"
+
+-- NEW IDEA: Append datatype.
+data MyList a = MyNil | MySingle a | MyAppend (MyList a) (MyList a)
+
+class MyElem (a :: k) (b :: MyList k)
+instance MyElem x (MySingle x)
+-- How to avoid these _duplicate_ instances???
+--instance MyElem x xs => MyElem x (MyAppend xs ys)
+--instance MyElem x xs => MyElem x (MyAppend ys xs)
+--instance MyElem x xs => MyElem x (MyAppend xs ys)
+--instance MyElem y ys => MyElem y (MyAppend xs ys)
+
+class Representative (rs :: [k]) (ts :: [k])
+instance Representative '[] '[]
+instance Representative rs ts => Representative (r ': rs) (r ': ts)
+instance (Representative rs ts, CElem t ts) => Representative rs (t ': ts)
+
+want :: (Representative rs ts, CElem t ts) => FList r rs -> (t -> r)
+want = undefined
+
+--instance (CElem x ss, Representative ts ss) => CElem x ts
+
+{- Indexed Either Section -}
+
+data IxEither ts a where
+  IxLeft :: OneOf ts -> IxEither ts a
+  IxRight :: a -> IxEither ts a
+
+-- TODO Should be able to give just a representative set.
+-- But how to code that to GHC?
+runIxEither :: FList r ts -> (a -> r) -> IxEither ts a -> r
+runIxEither flist f ixe = case ixe of
+  IxRight x -> f x
+  IxLeft oneof -> applyFList flist oneof
+
+data IxEitherS t a where
+  IxLeftS :: t -> IxEitherS t a
+  IxRightS :: a -> IxEitherS t a
+
+ixEitherSLift :: IxEitherS t a -> IxEither '[t] a
+ixEitherSLift ixes = case ixes of
+  IxRightS x -> IxRight x
+  IxLeftS y -> IxLeft (ThisOne y)
+
+ixMap :: (a -> b) -> IxEither ts a -> IxEither ts b
+ixMap f ixe = case ixe of
+  IxRight x -> IxRight (f x)
+  IxLeft y -> IxLeft y
+
+ixPure :: a -> IxEither ts a
+ixPure x = IxRight x
+
+ixAp :: IxEither ts (a -> b) -> IxEitherS t a -> IxEither (t ': ts) b
+ixAp ixe ixes = case ixe of
+  IxRight f -> case ixes of
+    IxRightS x -> IxRight (f x)
+    IxLeftS y -> IxLeft (ThisOne y)
+  IxLeft oneof -> case oneof of
+    ThisOne y -> IxLeft (ThisOne y)
+
+ixBind :: IxEitherS t a -> (a -> IxEither ts b) -> IxEither (t ': ts) b
+ixBind ixes k = case ixes of
+  IxLeftS y -> IxLeft (ThisOne y)
+  IxRightS x -> case k x of
+    IxRight x' -> IxRight x'
+    IxLeft (ThisOne y') -> IxLeft (ThisOne y')
+
+ixNext :: IxEither ts a -> IxEitherS t b -> IxEither (t ': ts) b
+ixNext ixe = ixAp (ixMap (flip const) ixe)
+
+(>>=) = ixBind
+(>>) = ixNext
+return = ixPure
+
+data SillyError = Kaboom
+data SeriousError = Blast
+
+exampleIx1 :: IxEitherS SillyError (Int -> Int -> Int)
+exampleIx1 = IxLeftS Kaboom
+
+exampleIx2 :: IxEitherS SeriousError Int
+exampleIx2 = IxLeftS Blast
+
+exampleIx3 :: Int -> IxEither ts Int
+exampleIx3 i = ixPure (i + 1)
+
+exampleIx4 :: IxEitherS SillyError Bool
+exampleIx4 = IxRightS True
+
+exampleIx = (ixEitherSLift exampleIx1) `ixAp` exampleIx2 `ixAp` exampleIx2
+
+exampleIx' = exampleIx2 `ixBind` exampleIx3
+
+exampleIx'' = do
+  x <- exampleIx2
+  exampleIx3 x
+  exampleIx4
